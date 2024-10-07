5b352ed7a2080d5ddd388485393b9d1687ba368f
hophiphip <nighop@yandex.ru>
2024-10-07 12:35:01 +0400

    chore: updated Vite JS build config for `react-fishbone`

--- packages/react-fishbone/vite.config.ts
+++ packages/react-fishbone/vite.config.ts
@@ -1,17 +1,56 @@
-import { resolve } from "node:path";
+import { extname, relative, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+import react from "@vitejs/plugin-react";
+import { glob } from "glob";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
+import packageJson from "./package.json";
+
+/** ------------------------------------------------------------------ */
+
 const sourceFolder = "src/";
-const libEntry = `${sourceFolder}index.ts`;
+const sourceEntries = `${sourceFolder}**/*.{ts,tsx}`;
+const sourceEntry = `${sourceFolder}index.ts`;
+
+const peerDependencies = Object.keys(packageJson.peerDependencies);
+
+const externalDependencies = [...peerDependencies, "react/jsx-runtime"];
+
+/** ------------------------------------------------------------------ */
+
+function getRelativePathToSourceFolder(file: string) {
+	return relative(sourceFolder, file.slice(0, file.length - extname(file).length));
+}
+
+const sourcesInput = Object.fromEntries(
+	glob
+		.sync(sourceEntries)
+		.map((file) => [
+			getRelativePathToSourceFolder(file),
+			fileURLToPath(new URL(file, import.meta.url)),
+		]),
+);
+
+/** ------------------------------------------------------------------ */
 
 export default defineConfig({
 	build: {
+		copyPublicDir: false,
 		lib: {
-			name: "fishbone-layout",
-			entry: resolve(__dirname, libEntry),
+			name: "react-fishbone",
+			entry: resolve(__dirname, sourceEntry),
 			formats: ["es"],
 			fileName: "index",
+		},
+		rollupOptions: {
+			external: externalDependencies,
+			input: sourcesInput,
+			output: {
+				assetFileNames: "assets/[name][extname]",
+				entryFileNames: "[name].js",
+			},
 		},
 	},
 	resolve: {
@@ -23,5 +62,6 @@ export default defineConfig({
 		dts({
 			tsconfigPath: "./tsconfig.lib.json",
 		}),
+		react(),
 	],
 });
